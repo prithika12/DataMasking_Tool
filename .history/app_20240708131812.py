3a1d18b0e714c75499ca267642c93f53bb1a620c
prithika12 <prithikamalesan@gmail.com>
2024-07-08 13:23:22 -0400

    first commit

---
+++ .history/app_20240708131812.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, send_file
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+# Function to mask data values
+def mask_data(input_file, output_file):
+    # Read the Excel file
+    df = pd.read_excel(input_file)
+
+    # Check if 'termInstance' column contains 'Confidential' or 'Restricted' and mask the 'data_value' column
+    df.loc[df['termInstance'].str.contains('Confidential|Restricted|Protected', case=False, na=False), 'data_value'] = 'XXXXXXX'
+
+    # Save the modified dataframe to a new Excel file
+    df.to_excel(output_file, index=False)
+
+@app.route('/')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        input_path = os.path.join('uploads', f.filename)
+        output_path = os.path.join('uploads', 'masked_' + f.filename)
+        
+        f.save(input_path)
+        mask_data(input_path, output_path)
+        
+        return send_file(output_path, as_attachment=True)
+    
+if __name__ == '__main__':
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+    app.run(debug=True)
